3d2cf053c1d225a963d0247fa41d0f36ef0b1cdf
Nibernaa Oryang Richard <niiric32@gmail.com>
2025-12-02 07:55:05 -0800

    chore: initial commit

---
+++ app/api/inngest/route.js
@@ -0,0 +1,14 @@
+import { serve } from "inngest/next";
+import { inngest } from "../../../inngest/client";
+import { syncUserDeletion, syncUserUpdation, synUserCreation } from "@/inngest/functions";
+
+// Create an API that serves zero functions
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+   //Adding all the users functions here
+   synUserCreation,
+   syncUserUpdation,
+   syncUserDeletion
+  ],
+});
---
+++ inngest/functions.js
@@ -0,0 +1,51 @@
+import {inngest} from './client'
+import prisma from '@/lib/prisma'
+
+//create an inngest function to save all user data
+
+export const synUserCreation = inngest.createFunction(
+    {id:'sync-user-create'},
+    {event:'clerk/user.created'},
+    async ({event})=>{
+        const {data}=event
+        await prisma.user.create(
+            {
+              data:{
+                   id:data.id,
+                   email:data.email_address[0].email_address,
+                   name:`${data.first_name} ${data.last_name}`,
+                   image:data.image_url
+              }
+            }
+        )
+    }
+)
+
+//Function for the user data update
+export const syncUserUpdation=inngest.createFunction(
+    {id:'syn-user-update'},
+    {event:'clerk/user.updated'},
+    async({event})=>{
+        const {data}=event
+        await prisma.user.update({
+            where:{id:data.id,},
+            data:{
+                email:data.email_address[0].email_address,
+                name:`${data.first_name} ${data.last_name}`,
+                image:data.image_url
+            }
+        })
+    }
+)
+
+//Delete a user from the database
+export const syncUserDeletion=inngest.createFunction(
+    {id:'syn-user-delete'},
+    {event:'clerk/user.deleted'},
+    async({event})=>{
+        const {data}=event
+        await prisma.user.delete({
+            where:{id:data.id,}
+        })
+    }
+)
